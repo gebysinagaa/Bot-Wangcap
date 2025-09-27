75fa9bf5eb8d01d9c3ee79902f3daf3b72bc5c1e
gebysinagaaa________10 <marszllbot21@gmail.com>
2025-09-28 01:47:12 +0700

    Add files via upload

---
+++ command/marsel.js
@@ -0,0 +1,17 @@
+/*
+┏╼━━━━━━━━━━━━━━━━━━╾┓
+BASE : https://github.com/AlifatahFauzi
+RECODE : t.me/marsellmawujadidev 
+CH : https://whatsapp.com/channel/0029VbBFzoWDDmFYnclusM13
+┗╼━━━━━━━━━━━━━━━━━━╾┛
+*/
+
+let handler = async (m, { reply }) => {
+    reply("> Marselsukisok asik 😹");
+};
+
+handler.help = ['yes'];
+handler.tags = ['main'];
+handler.command = ["yes"];
+
+module.exports = handler;
